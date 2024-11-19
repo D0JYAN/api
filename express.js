2051c69a87d86f2f6c52ea6cfd34ed1e95552078
JIELSAN <jesussjg811@gmail.com>
2024-11-19 00:17:15 -0600

    middleware de post

--- express.js
+++ express.js
@@ -9,11 +9,18 @@ const futaba = require('./bunny-girl/futaba.json');
 
 const PORT = process.env.PORT ?? 1234;
 
-app.get('/bunny-girl/futaba', (req, res) => {
-    res.json(futaba);
+//Middleware
+app.use((req, res, next) => {
+    console.log('primer middleware');
+    //Trackear la request a la BD
+    //Revisar si el usuario tiene cookies
+    next()//Esto es esencial para continuar con las peticiones
 })
 
-app.post('/bunny-girl', (req, res) => {
+app.use((req, res, next) => {
+    if(req.method !== 'POST') return next();
+    if(req.headers['content-type'] !== 'application/json') return next();
+
     let body = ''
     //Escuchar el evento data
     req.on('data', chunk => {
@@ -21,9 +28,19 @@ app.post('/bunny-girl', (req, res) => {
     })
     req.on('end', () => {
         const data = JSON.parse(body);
-        data.timestamp =Date.now();
-        res.status(201).json(data);
+        data.timestamp = Date.now();
+        //mutar la request y meter la info en el req.body
+        req.body = data;
+        next();
     })
+})
+
+app.get('/bunny-girl/futaba', (req, res) => {
+    res.json(futaba);
+})
+
+app.post('/bunny-girl', (req, res) => {
+    res.status(201).json(req.body);
 })
 
 //Esta es la ultima ruta que va a usar, 
